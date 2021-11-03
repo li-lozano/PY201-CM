14e8eb4f10c1cbb36737c9421ed1509e8f9ccc93
Lizandro Lozano <llozano@outlook.com>
2021-11-03 16:43:04 -0500

    complete CRUD

--- app/contact.py
+++ app/contact.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from werkzeug.exceptions import abort
 from app.db import get_db
 
 bp = Blueprint('contact',__name__, url_prefix='/')
@@ -47,11 +48,57 @@ def create():
                     flash(error)
     return render_template('contacts/create.html')
 
+def get_contact(id):
+    db, c = get_db()
+    c.execute(
+        'SELECT * FROM contacts WHERE id = %s', (id,)
+    )
+    contact = c.fetchone()
+
+    if contact is None:
+        abort(404,'El contacto con el id {0} no existe'.format(id))
+    return contact
+
 @bp.route('<int:id>/update', methods=['GET', 'POST'])
 def update(id):
-    return 'actualizar'
+    contact = get_contact(id)
 
-@bp.route('/delete/<int:id>', methods=['GET', 'POST'])
+    if request.method == 'POST':
+            company = request.form.get('company')
+            role = request.form.get('role')
+            firstname = request.form.get('firstname')
+            lastname = request.form.get('lastname')
+            mail = request.form.get('mail')
+            phone = request.form.get('phone')
+            errors = []
+
+            if not company:
+                errors.append('El campo empresa no puede estar en blanco')
+            if not role:
+                errors.append('El campo rol no puede estar en blanco')
+            if not firstname:
+                errors.append('El campo nombre no puede estar en blanco')
+            if not lastname:
+                errors.append('El campo apellido no puede estar en blanco')
+            if not mail:
+                errors.append('El campo correo no puede estar en blanco')
+            if not phone:
+                errors.append('El campo telefono no puede estar en blanco')
+    
+            if len(errors) == 0:
+                db, c = get_db()
+                c.execute(
+                    'UPDATE contacts SET company = %s, role = %s, firstname = %s, lastname = %s, mail = %s, phone = %s WHERE id = %s',
+                    (company, role, firstname, lastname, mail, phone, id)
+                )
+                db.commit()
+                return redirect(url_for('contact.home'))
+            else:
+                for error in errors:
+                    flash(error)
+    return render_template('contacts/update.html', contact = contact)
+
+@bp.route('<int:id>/delete', methods=['GET', 'POST'])
 def delete(id):
         db, c = get_db()
         c.execute(
